aabc578a671d4a2d5334bbaa3f6cadd20292b4c1
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-05-21 17:53:14 -0700

    Add OG image generation

---
+++ app/api/og/route.tsx
@@ -0,0 +1,90 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const hasTitle = searchParams.has('title')
+  const title = hasTitle
+    ? searchParams.get('title')
+    : 'MDX on Next.js 14'
+
+  const imageData = await fetch(
+    new URL('../../../public/og-background.png', import.meta.url))
+    .then((res) => res.arrayBuffer())
+
+  const GeistRegular = await fetch(
+    new URL('../../../public/fonts/Geist-Regular.otf', import.meta.url))
+    .then((res) => res.arrayBuffer())
+
+  const GeistBold = await fetch(
+    new URL('../../../public/fonts/Geist-Bold.otf', import.meta.url))
+    .then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          backgroundColor: 'white',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            top: '45%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+            fontSize: 60,
+            lineHeight: 1.6,
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontFamily: '"Geist Bold"',
+          }}
+        >
+          {title}
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          top: '55%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          color: 'white',
+          fontSize: 30,
+          lineHeight: 1.6,
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontFamily: '"Geist Regular"',
+        }}
+      >
+        A page from MDX on Next.js 14
+      </div>
+    </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'Geist Regular',
+          data: GeistRegular,
+          style: 'normal'
+        },
+        {
+          name: 'Geist Bold',
+          data: GeistBold,
+          style: 'normal'
+        }
+      ]
+    }
+  )
+}
