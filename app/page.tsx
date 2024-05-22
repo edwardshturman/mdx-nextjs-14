16f9cda0a3a9785794fb48ac18f2cdd4c37b361b
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-05-21 17:43:31 -0700

    Add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
@@ -5,6 +7,19 @@ export default function Home() {
       <p>
         An ergonomic, performant MDX setup for your Next.js 14 app, fit with YAML frontmatter parsing, dynamic metadata &amp; OG image generation, and static site generation
       </p>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/hello-world"}>Hello World</Link>
+          </li>
+          <li>
+            <Link href={"/features"}>Features</Link>
+          </li>
+          <li>
+            <Link href={"/next-steps"}>Next Steps</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
